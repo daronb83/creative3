4bdc178ebf69aa1299c90168f0f3db50f99d7a97
csweb2 <webmaster@cs.byu.edu>
2017-10-19 14:58:05 -0600

    push2

---
+++ char.js
@@ -0,0 +1,98 @@
+// App
+var charApp = angular.module('Character', ['ui.router'])
+
+// Configuration
+var config = charApp.config([
+  '$stateProvider',
+  '$urlRouterProvider',
+
+  function($stateProvider, $urlRouterProvider) {
+    var home = $stateProvider.state('home', {
+        url: '/home',
+        templateUrl: '/home.html',
+        controller: 'MainCtrl'
+    });
+
+    var c = $stateProvider.state('characters', {
+      url: '/characters/{id}',
+      templateUrl: '/characters.html',
+      controller: 'charactersCtrl'
+    });
+
+    $urlRouterProvider.otherwise('home');
+  }
+]);
+
+// Factory
+var factory = charApp.factory('charFactory', [
+  function() {
+    var o = {
+      chars: []
+    };
+
+    return o;
+  }
+]);
+
+// Main controller
+var mainCtrl = charApp.controller('MainCtrl', [
+  '$scope',
+  'charFactory',
+
+  function($scope, charFactory) {
+    $scope.chars = charFactory.chars;
+    $scope.genders = ["male", "female"];
+
+    $scope.addCharacter = function(){
+      if($scope.name === '') {
+        return;
+      }
+
+      $scope.chars.push({
+        name: $scope.name,
+        gender: $scope.gender,
+        attributes: {}
+      });
+
+      console.log(charFactory.chars);
+
+      $scope.name = '';
+      $scope.gender = '';
+    };
+  }
+]);
+
+// characters controller
+var charactersCtrl = charApp.controller('charactersCtrl', [
+  '$scope',
+  '$stateParams',
+  'charFactory',
+
+  function($scope, $stateParams, charFactory) {
+    $scope.char = charFactory.chars[$stateParams.id];
+
+    $scope.hairColors = ["Black", "Brown", "Yellow", "Red", "White"];
+    $scope.eyes = ["Green", "Blue", "Brown", "Gray"];
+    $scope.skinColors = ["Fair", "Tan", "Black"];
+    $scope.outfits = ["Simple", "Woodsy", "Comfy", "Formal"];
+
+    $scope.addAttributes = function(){
+      //console.log("stateParams: " + $stateParams.id);
+      //console.log($scope.char);
+
+      if($scope.hair === '') {
+        return;
+      }
+
+      $scope.char.attributes = {
+        hair: $scope.hair,
+        eye: $scope.eye,
+        skin: $scope.skin,
+        outfit: $scope.outfit,
+        personality:$scope.personality
+      };
+
+      //console.log($scope.char.attributes);
+    };
+  }
+]);
